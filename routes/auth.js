36c122bd94f358fdf746fa0b667fb587e1bf153b
ALCHURI SAI PRANEETH REDDY <87567651+saipran23@users.noreply.github.com>
2026-06-19 16:41:35 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,15 @@
 
 import express from "express";
 
-import {register, login} from "../controllers/auth-controllers.js";
+import {register, login, changePassword} from "../controllers/auth-controllers.js";
+import authMiddleware from "../middleware/auth-middleware.js";
 
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/changePassword", authMiddleware, changePassword);
 
 
 
